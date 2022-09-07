6339e6451ba883658d94305e04f2351b90cdab96
zbthompson93 <zbthompson93@penguin>
2022-09-07 16:21:14 -0400

    got stats from table

--- get_data.py
+++ get_data.py
@@ -16,6 +16,8 @@ players = []
 soup = BeautifulSoup(text, features="html.parser")
 #print(soup)
 
+team = "atl"
+
 '''Get the data from the name table'''
 # Get name table
 nameTable = soup.find('table', class_='Table Table--align-right Table--fixed Table--fixed-left')
@@ -30,10 +32,35 @@ for name in names:
     players.append(obj)
     #print(name.text)
 
+
+'''Get the data from the stats table'''
+# Get stats table
+statsTable = soup.find('table', class_='Table Table--align-right')
+
+# Get headers for stats list
+statsList = statsTable.find('thead', class_='Table__header-group Table__THEAD')
+statsName = statsList.find_all('th')
+
+# Get table where stats are listed for each player
+statsBody = statsTable.find('tbody', class_='Table__TBODY')
+# Get the rows from that stats table to loop thru
+statsRows = statsBody.find_all('tr')
+
 # Get player positions and update players list
 pos = nameTable.find_all('span', class_='font10')
+# Loop thru each position
 for count, position in enumerate(pos):
     obj = players[count]
     obj.update({"position": position.contents[2]})
+    obj.update({"team": team.upper()})
 
-print(players)
+    # Get the matching row in the stats body table
+    row = statsRows[count].find_all('td')
+
+    #Add stats headers as keys and the stats from the rows var to each player object
+    for count2, stats in enumerate(statsName):
+        stat = row[count2].string
+        obj.update({stats.string: stat})
+
+
+print(players[9])
